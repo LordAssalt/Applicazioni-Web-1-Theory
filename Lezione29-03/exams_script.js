41057b485c8c2da570aafecc647ebbf94c8da0c8
Alex Carluccio <alexcarluccio@live.it>
2022-03-29 11:17:26 +0200

    Implementazione degli eventi e di JS con HTML

---
+++ Lezione29-03/exams_script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let p = document.createElement('p');
+let d = dayjs().format('YYYY-MM-DD HH:mm:ss');
+p.innerText = d;
+
+document.getElementById('ora').prepend(p);
+
+setInterval(() => { p.innerText = dayjs().format('YYYY-MM-DD HH:mm:ss'); }, 1000);
+
+//.innerText = "qui ci va l'ora";
+
+window.addEventListener("load", event => {
+    const rows = document.querySelectorAll('table tr');
+    for (const row of rows) {
+        row.addEventListener("click", event => {
+            console.log(event.target, row);
+            const voto = event.target.innerText;
+            const p= document.createElement("p");
+            p.innerText=voto;
+            document.getElementById("comment").appendChild(p);
+
+        })
+    }
+});
+
+
