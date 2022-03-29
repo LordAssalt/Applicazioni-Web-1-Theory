7f06447f1a5041465180070af6b17ede287c56b3
Alex Carluccio <alexcarluccio@live.it>
2022-03-29 11:31:15 +0200

    Implementazione dell'eliminazione delle righe nella tabella

--- Lezione29-03/exams_script.js
+++ Lezione29-03/exams_script.js
@@ -13,15 +13,25 @@ setInterval(() => { p.innerText = dayjs().format('YYYY-MM-DD HH:mm:ss'); }, 1000
 window.addEventListener("load", event => {
     const rows = document.querySelectorAll('table tr');
     for (const row of rows) {
-        row.addEventListener("click", event => {
+        /*row.addEventListener("click", event => {
             console.log(event.target, row);
             const voto = event.target.innerText;
-            const p= document.createElement("p");
-            p.innerText=voto;
+            const p = document.createElement("p");
+            p.innerText = voto;
             document.getElementById("comment").appendChild(p);
 
-        })
+        })*/
+
+        if (row.firstElementChild.tagName.toLowerCase() !== "th") {
+            row.querySelector('.btn').addEventListener("click", event => {
+                while (row.firstChild)
+                    row.removeChild(row.firstChild);
+                const parent = row.parentNode;
+                parent.removeChild(row);
+            })
+        }
     }
+
 });
 
 
